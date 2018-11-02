a1a522a8a89be11cf39e7758f69b782cec2f7cd6
Longraf <13Longraf@mail.ru>
2018-11-02 17:31:35 +0300

    somesing Node.js

---
+++ Node/my_parser.js
@@ -0,0 +1,33 @@
+// загружаем модуль filesystem(fs)
+var fs = require('fs');
+ 
+// считываем содержание файла в память
+fs.readFile('example_log.txt', function (err, logData) {
+ 
+    // Если возникла ошибка, мы кидаем исключение
+	// и программа заканчивается
+    if (err) throw err;
+ 
+    // logData имеет тип Buffer, конвертируем в строку
+    var text = logData.toString();
+ 
+var results = {};
+ 
+// Разбиваем текст на массив из строчек
+var lines = text.split('\n');
+ 
+lines.forEach(function(line) {
+    var parts = line.split(' ');
+    var letter = parts[1];
+    var count = parseInt(parts[2]);
+ 
+if(!results[letter]) {
+      results[letter] = 0;
+}
+ 
+results[letter] += parseInt(count);
+});
+ 
+console.log(results);
+// { A: 2, B: 14, C: 6 }
+});
